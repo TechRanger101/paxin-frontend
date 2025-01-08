247f485c3e61c350db8a4b54c28428e892e8b6fe
TechRanger101 <danielbertell101@gmail.com>
2025-01-08 19:20:14 -0400

    tvplayer 2

--- components/VideoPlayerHls.tsx
+++ components/VideoPlayerHls.tsx
@@ -19,15 +19,18 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, poster }) => {
     <MediaPlayer
       streamType='live'
       title='Custom title'
-      src='https://app.ddrw.org/api/v1/streams/d8e8f724-9954-48cc-9463-cc3d3e46b0fd/stream.m3u8'
+      src={{
+        src: 'https://app.ddrw.org/api/v1/streams/d8e8f724-9954-48cc-9463-cc3d3e46b0fd/stream.m3u8',
+        type: 'application/vnd.apple.mpegurl',
+      }}
       autoPlay
       playsInline
     >
-      {/* <MediaProvider />
+      <MediaProvider />
       <DefaultVideoLayout
         thumbnails='https://files.vidstack.io/sprite-fight/thumbnails.vtt'
         icons={defaultLayoutIcons}
-      /> */}
+      />
     </MediaPlayer>
   );
 };
