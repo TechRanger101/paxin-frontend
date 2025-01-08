4f2878efd201b1b74bf7b481523be5c53f2dc6dc
TechRanger101 <danielbertell101@gmail.com>
2025-01-08 16:57:45 -0400

    tvplayer 2

--- components/VideoPlayerHls.tsx
+++ components/VideoPlayerHls.tsx
@@ -19,6 +19,8 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ src, poster }) => {
     <MediaPlayer
       title='Sprite Fight'
       src='https://app.ddrw.org/api/v1/streams/d8e8f724-9954-48cc-9463-cc3d3e46b0fd/stream.m3u8'
+      autoPlay
+      playsInline
     >
       <MediaProvider />
       <DefaultVideoLayout
